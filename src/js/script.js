914f6a8d00d030abe27e99efc5e9d319ddcc09aa
Mohajiro <lavrovuriy@gmail.com>
2024-11-26 14:12:53 +0100

    Votre message de commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,23 @@
-const burger = document.querySelector('.burger');
-const sidebar = document.querySelector('.sidebar');
+fetch('../pages/header.html')
+  .then(response => {
+    if(!response.ok) {
+      throw new Error(`Erreur de telecharegement ${response.status}`);
+    }
+    return response.text();
+  })
+.then(data => {
+    document.getElementById('header').innerHTML = data;
+  })
+.catch(error => console.error('Erreur:', error));
 
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  sidebar.classList.toggle('active');
-});
+fetch('../pages/sidebar.html')
+  .then(response => {
+    if(!response.ok) {
+      throw new Error(`Erreur de telecharegement ${response.status}`);
+    }
+    return response.text();
+  })
+.then(data => {
+    document.getElementById('sidebar').innerHTML = data;
+  })
+.catch(error => console.error('Erreur:', error));
