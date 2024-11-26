d9561ffc9efb83905542cff24ee9f879622f2012
Dzmitri <dmardovitch@gmail.com>
2024-11-26 16:30:38 +0100

    Dernier pour aujourd'hui

--- src/js/modules/project.js
+++ src/js/modules/project.js
@@ -85,3 +85,28 @@ document.addEventListener('DOMContentLoaded', () => {
     displayRepoContents(repoData);
   })
   .catch(error => console.error('Error fetching data:', error));
+
+
+  fetch('../pages/header.html')
+  .then(response => {
+    if(!response.ok) {
+      throw new Error(`Erreur de telecharegement ${response.status}`);
+    }
+    return response.text();
+  })
+.then(data => {
+    document.getElementById('header').innerHTML = data;
+  })
+.catch(error => console.error('Erreur:', error));
+
+fetch('../pages/sidebar.html')
+  .then(response => {
+    if(!response.ok) {
+      throw new Error(`Erreur de telecharegement ${response.status}`);
+    }
+    return response.text();
+  })
+.then(data => {
+    document.getElementById('sidebar').innerHTML = data;
+  })
+.catch(error => console.error('Erreur:', error));
